3a29b3a04510e34cb01298e17635ee5bc7500720
mostafasamer1999 <mostafasamer1999@hotmail.com>
2021-05-20 00:54:40 +0200

    Add BL

---
+++ IAProject-FreelancerSystem/BL/SavedJobs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IAProject_FreelancerSystem.BL
+{
+    public class SavedJobs
+    {
+        public int savedID { set; get; }
+        public int freelancerID { set; get; }
+        public int jobID { set; get; }
+        public string jobTitle { set; get; }
+        public string jobDate { set; get; }
+        public string clientName { set; get; }
+    }
+}
