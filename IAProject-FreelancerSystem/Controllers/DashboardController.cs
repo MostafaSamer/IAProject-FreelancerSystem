0e9de714904446124ad1d2994140338f8f7fcfaa
mostafasamer1999 <mostafasamer1999@hotmail.com>
2021-05-21 01:54:01 +0200

    Add Routing

---
+++ IAProject-FreelancerSystem/Controllers/DashboardController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IAProject_FreelancerSystem.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult UsersPage()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult PostsPage()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult PostsRequests()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
